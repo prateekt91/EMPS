2af9364bd989fce4ebeaeefbf0abec108dbbb004
Prateek Tiwari <prateekt91@gmail.com>
2024-10-23 18:08:39 +0530

    [added loggers]

--- apiintegrationservice/src/main/java/com/prat/apiintegrationservice/service/TempApiServiceImpl.java
+++ apiintegrationservice/src/main/java/com/prat/apiintegrationservice/service/TempApiServiceImpl.java
@@ -4,10 +4,11 @@ import com.prat.apiintegrationservice.model.Temperature;
 import com.prat.apiintegrationservice.model.TemperatureResponse;
 import com.prat.apiintegrationservice.repository.CustomTempRepo;
 import com.prat.apiintegrationservice.repository.TemperatureRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,7 @@ import java.time.Duration;
 import java.time.Instant;
 
 @Service
-public class TempApiServiceImpl implements TempApiService{
+public class TempApiServiceImpl implements TempApiService {
 
     @Value("${api.air-quality.base-url}")
     private String baseUrl;
@@ -36,6 +37,8 @@ public class TempApiServiceImpl implements TempApiService{
     @Autowired
     private CustomTempRepo customTempRepo;
 
+    private static final Logger log = LoggerFactory.getLogger(TempApiServiceImpl.class);
+
     @Override
     public Temperature getCurrentTempurature(double lat, double lon) {
         return customTempRepo.getLatestTemperature(lat, lon);
@@ -44,12 +47,16 @@ public class TempApiServiceImpl implements TempApiService{
     @Override
     public Temperature getTemperatureFromService(double lat, double lon) {
 
+        String methodName = "TempApiServiceImpl.getTemperatureFromService";
+        log.info("Inside method {} ", methodName);
         RestTemplate restTemplate = restTemplateBuilder
                 .setConnectTimeout(Duration.ofSeconds(5))
                 .setReadTimeout(Duration.ofSeconds(5))
                 .basicAuthentication(apiUserName, apiPassword)
                 .build();
         String temperatureUrl = baseUrl + "/" + Instant.now().toString() + "/t_2m:C/" + lat + "," + lon + "/json";
+
+        log.info("Calling temp api with url: {}", temperatureUrl);
 
         ResponseEntity<TemperatureResponse> response = restTemplate.exchange(temperatureUrl, HttpMethod.GET, null, TemperatureResponse.class);
 
